91df72907d4b5dff372dc38a3881ef8009998aef
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-02-26 08:53:43 +0100

    Fix linting

--- shopfloor_mobile/static/wms/src/components/searchbar/searchbar.js
+++ shopfloor_mobile/static/wms/src/components/searchbar/searchbar.js
@@ -10,26 +10,26 @@ Vue.component('searchbar', {
     },
     props:['input_placeholder', 'input_data_type'],
     methods: {
-  	search: function (e, ) {
-  		e.preventDefault();
+        search: function (e, ) {
+            e.preventDefault();
             this.$emit('found', {
                 text: this.entered,
                 type: e.target.dataset.type,
             }); // Talk to parent
             this.reset();
         },
-  	reset: function () {
-  		this.entered = '';
-  	},
+        reset: function () {
+            this.entered = '';
+        },
     },
 
     template: `
-  <v-form
-      v-on:submit="search"
-      :data-type="input_data_type"
-      ref="form"
-      >
+    <v-form
+    v-on:submit="search"
+    :data-type="input_data_type"
+    ref="form"
+    >
     <v-text-field required v-model="entered" :placeholder="input_placeholder" :autofocus="autofocus ? 'autofocus' : null"></v-text-field>
-  </v-form>
-  `,
+    </v-form>
+    `,
 });
