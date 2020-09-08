eb443439d0d84b0df568bdc218740c0562fe18ef
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-09-08 08:46:21 +0200

    backend: delete package level when changing result_package_id
    When we set a result_package_id directly on a move line, we are no
    longer moving the entire package, we should therefore remove the package
    level. Otherwise, we can have unexpected effects later on.

--- shopfloor/models/stock_package_level.py
+++ shopfloor/models/stock_package_level.py
@@ -33,3 +33,29 @@ class StockPackageLevel(models.Model):
                     line.owner_id = quant.owner_id
 
         self.package_id = new_package
+
+    # TODO could be called when we write a different result package on line?
+    def shallow_unlink(self):
+        """Unlink package level without affecting moves and lines
+
+        It still removes the result_package_id of related move lines,
+        but *only* when it is the same package (prevent to remove a
+        package changed manually).
+        """
+        if not self:
+            return True
+        for package_level in self:
+            # We are no longer moving the entire package, match odoo's behavior
+            # by emptying the result package, but keep it if it had a different
+            # value set, because it means it has been set.
+            lines = package_level.move_line_ids.filtered(
+                lambda ml: ml.result_package_id == package_level.package_id
+            )
+            lines.result_package_id = False
+        # when we unlink a package level, it automatically drops
+        # any related move and resets the result_package_id of ALL
+        # move lines, we prevent this by detaching it first
+        self.write({"move_ids": [(6, 0, [])], "move_line_ids": [(6, 0, [])]})
+        # as we are no longer moving an entire package, the
+        # package level is irrelevant
+        return self.unlink()
