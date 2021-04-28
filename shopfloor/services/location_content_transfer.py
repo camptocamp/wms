d1820c6dbf0d62324581bd35f2aba4c92a389d8d
Jacques-Etienne Baudoux <je@bcim.be>
2021-04-28 16:30:41 +0200

    Fix single pack transfer - check valid destination
    We ensure the destination is either valid regarding the picking
    destination location or the move destination location. With the push
    rules in the module stock_dynamic_routing in OCA/wms, it is possible
    that the move destination is not anymore a child of the picking default
    destination (as it is the last pushed move that now respects this
    condition and not anymore this one that has a destination to an
    intermediate location)

--- shopfloor/services/location_content_transfer.py
+++ shopfloor/services/location_content_transfer.py
@@ -1,4 +1,5 @@
-# Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
+# Copyright 2020-2021 Camptocamp SA (http://www.camptocamp.com)
+# Copyright 2020-2021 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import _
 
