f4b6aa57bc82588bfcb17239da9ce50a9891b05d
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-12-09 09:33:36 +0100

    sf_w_label_printer: leverage base_report_to_label_printer'
    Module `base_report_to_label_printer` allows to flag reports as labels
    and provides a method to get the expected label printer for a given
    user. So:
    
        - Deprecate the `res.users.get_delivery_label_printer` method.
          A better alternative is to use `ir.actions.report._get_user_default_printer`.
        - Remove the 'res.users.default_label_printer_id' field definition
          from the current module as it already exists in
          `base_report_to_label_printer`.

---
+++ shopfloor_workstation_label_printer/migrations/14.0.1.1.0/pre-migration.py
@@ -0,0 +1,35 @@
+# Copyright 2022 Camptocamp SA (http://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from openupgradelib import openupgrade as ou
+except ImportError as exc:
+    _logger.debug(exc)
+    ou = None
+
+
+def migrate(cr, version):
+    """Move resources from 'shopfloor_workstation_label_printer'."""
+    if not version:
+        return
+    if not ou:
+        return
+    old_module = "shopfloor_workstation_label_printer"
+    new_module = "base_report_to_label_printer"
+    ou.update_module_moved_fields(
+        "res.users", ["default_label_printer_id"], old_module, new_module
+    )
+    ou.rename_xmlids(
+        cr,
+        [
+            (f"{old_module}.view_users_form", f"{new_module}.view_users_form"),
+            (
+                f"{old_module}.view_users_form_simple_modif",
+                f"{new_module}.view_users_form_simple_modif",
+            ),
+        ],
+    )
