7fe8590a003d402993838cdd7d77efd7a0467573
Sébastien Alix <sebastien.alix@camptocamp.com>
2023-01-30 11:23:03 +0100

    shopfloor, checkout: allow change lot on line select
    When it comes to select a line, scanning the wrong lot will invite the
    user to scan it again to change it on the relevant line only if there is
    one sharing the same product.

--- shopfloor/actions/message.py
+++ shopfloor/actions/message.py
@@ -106,6 +106,15 @@ class MessageAction(Component):
             ),
         }
 
+    def lot_different_change(self):
+        return {
+            "message_type": "warning",
+            "body": _(
+                "You scanned a different lot with the same product, "
+                "do you want to change lot? Scan it again to confirm."
+            ),
+        }
+
     def package_not_available_in_picking(self, package, picking):
         return {
             "message_type": "warning",
@@ -845,4 +854,16 @@ class MessageAction(Component):
         return {
             "message_type": "warning",
             "body": _("No line to pack found."),
+        }
+
+    def lot_changed(self):
+        return {
+            "message_type": "info",
+            "body": _("Lot changed"),
+        }
+
+    def lot_change_no_line_found(self):
+        return {
+            "message_type": "error",
+            "body": _("Unable to find a line with the same product but different lot."),
         }
