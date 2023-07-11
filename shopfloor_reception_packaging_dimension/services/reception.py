0b61a831856f05a5e833293f44a0cea590594b73
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2023-07-11 08:06:26 +0200

    sh_reception_packaging_dimension: refactor to allow module to extend

--- shopfloor_reception_packaging_dimension/services/reception.py
+++ shopfloor_reception_packaging_dimension/services/reception.py
@@ -1,6 +1,8 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
+from odoo.osv import expression
+
 from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
 from odoo.addons.shopfloor.utils import to_float
@@ -8,6 +10,12 @@ from odoo.addons.shopfloor.utils import to_float
 
 class Reception(Component):
     _inherit = "shopfloor.reception"
+
+    @property
+    def dimension_to_update(self):
+        if not hasattr(self.work, "dimension_to_update"):
+            self.work.dimension_to_update = {}
+        return self.work.dimension_to_update
 
     def _before_response_for_set_quantity(self, picking, line):
         """Show the packaging dimension screen before the set quantity screen."""
@@ -18,12 +26,8 @@ class Reception(Component):
             return self._response_for_set_packaging_dimension(picking, line, packaging)
         return self._response_for_set_quantity(picking, line)
 
-    def _get_next_packaging_to_set_dimension(self, product, previous_packaging=None):
-        """Return for a product the next packaging needing dimension to be set."""
-        next_packaging_id = previous_packaging.id + 1 if previous_packaging else 0
-        domain = [
-            ("product_id", "=", product.id),
-            ("id", ">=", next_packaging_id),
+    def _get_domain_packaging_needs_dimension(self):
+        return [
             "|",
             "|",
             "|",
@@ -46,6 +50,16 @@ class Reception(Component):
             ("qty", "=", False),
             ("barcode", "=", False),
         ]
+
+    def _get_next_packaging_to_set_dimension(self, product, previous_packaging=None):
+        """Return for a product the next packaging needing dimension to be set."""
+        next_packaging_id = previous_packaging.id + 1 if previous_packaging else 0
+        domain_dimension = self._get_domain_packaging_needs_dimension()
+        domain_packaging_id = [
+            ("product_id", "=", product.id),
+            ("id", ">=", next_packaging_id),
+        ]
+        domain = expression.AND([domain_packaging_id, domain_dimension])
         return self.env["product.packaging"].search(domain, order="id", limit=1)
 
     def _response_for_set_packaging_dimension(
@@ -64,6 +78,19 @@ class Reception(Component):
             },
             message=message,
         )
+
+    def _store_dimension_to_update(self, dimensions, local_table):
+        """Store values to update in the context manager of the component.
+
+        It helps for extending the module to add more dimension fields to be updated.
+
+        """
+        for dimension in dimensions:
+            if local_table[dimension] is None:
+                if dimension in self.dimension_to_update.keys():
+                    self.dimension_to_update.pop(dimension)
+            else:
+                self.dimension_to_update[dimension] = local_table[dimension]
 
     def set_packaging_dimension(
         self,
@@ -87,6 +114,9 @@ class Reception(Component):
             - set_packaging_dimension: if more packaging needs dimension
             - set_quantity: otherwise
         """
+        self._store_dimension_to_update(
+            ["height", "length", "width", "weight", "barcode", "qty"], locals()
+        )
         picking = self.env["stock.picking"].browse(picking_id)
         selected_line = self.env["stock.move.line"].browse(selected_line_id)
         packaging = self.env["product.packaging"].sudo().browse(packaging_id)
@@ -94,19 +124,8 @@ class Reception(Component):
         next_packaging = None
         if not packaging:
             message = self.msg_store.record_not_found()
-        elif (height or length or width or weight or barcode or qty) and not cancel:
-            if height:
-                packaging.height = height
-            if length:
-                packaging.packaging_length = length
-            if width:
-                packaging.width = width
-            if weight:
-                packaging.max_weight = weight
-            if barcode:
-                packaging.barcode = barcode
-            if qty:
-                packaging.qty = qty
+        elif not cancel and self._check_dimension_to_update():
+            self._update_packaging_dimension(packaging)
             message = self.msg_store.packaging_dimension_updated(packaging)
         if packaging:
             next_packaging = self._get_next_packaging_to_set_dimension(
@@ -119,6 +138,25 @@ class Reception(Component):
         return self._response_for_set_packaging_dimension(
             picking, selected_line, next_packaging, message=message
         )
+
+    def _check_dimension_to_update(self):
+        """Return True if there is any dimension that needs to be updated on the packaging."""
+        dimension_to_check = ["height", "length", "width", "weight", "barcode", "qty"]
+        return any(
+            [self.dimension_to_update.get(dim, False) for dim in dimension_to_check]
+        )
+
+    def _update_packaging_dimension(self, packaging):
+        """Update dimension on the packaging."""
+        dimension_to_update = ["height", "length", "width", "weight", "barcode", "qty"]
+        for dimension in dimension_to_update:
+            value = self.dimension_to_update.get(dimension, None)
+            if value is not None:
+                if dimension == "weight":
+                    dimension = "max_weight"
+                elif dimension == "length":
+                    dimension = "packaging_length"
+                packaging[dimension] = value
 
     class ShopfloorReceptionValidator(Component):
         _inherit = "shopfloor.reception.validator"
