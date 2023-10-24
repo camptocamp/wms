0aeb55f6792a19ead71c72f79edcc9c060666178
JuMiSanAr <jumisan134@gmail.com>
2023-10-24 11:07:54 +0200

    shopfloor: add option to order the picking list by priority

--- shopfloor/tests/test_checkout_base.py
+++ shopfloor/tests/test_checkout_base.py
@@ -27,8 +27,8 @@ class CheckoutCommonCase(CommonCase):
         return self.service._data_for_stock_picking(picking, **kw)
 
     # we test the methods that structure data in test_actions_data.py
-    def _picking_summary_data(self, picking):
-        return self.data.picking(picking)
+    def _picking_summary_data(self, picking, **kw):
+        return self.data.picking(picking, **kw)
 
     def _move_line_data(self, move_line):
         return self.data.move_line(move_line)
