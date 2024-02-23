f7f194e7b27247fcb507ae9c0bae96e571a3de6d
Jacques-Etienne Baudoux <je@bcim.be>
2024-02-23 17:04:12 +0100

    [ADD] stock_release_channel_show_volume
    Co-authored-by: Michael Tietz (MT Software) <mtietz@mt-software.de>

---
+++ stock_release_channel_show_volume/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Stock Release Channels show Volumes",
+    "summary": "Displays volumes of stock release channels",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "MT-Software, BCIM, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "depends": [
+        "stock_picking_volume",
+        "stock_release_channel",
+    ],
+    "data": [
+        "views/release_channel.xml",
+    ],
+    "installable": True,
+}
