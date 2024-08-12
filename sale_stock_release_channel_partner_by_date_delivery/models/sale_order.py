e433a2b159f0eba04aa4df5b5cd919c83f78d01a
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-08-12 13:40:54 +0200

    fixup! sale_stock_release_channel_partner_by_date_delivery: glue module with delivery

--- sale_stock_release_channel_partner_by_date_delivery/models/sale_order.py
+++ sale_stock_release_channel_partner_by_date_delivery/models/sale_order.py
@@ -1,19 +1,19 @@
 # Copyright 2024 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import models
+from odoo.osv import expression
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    release_channel_id = fields.Many2one(
-        domain=(
-            "carrier_id and "
-            "['|', ('carrier_ids', '=', False), ('carrier_ids', 'in', [carrier_id])] "
-            "or []"
+    def _release_channel_id_domain_parts(self):
+        domain = super()._release_channel_id_domain_parts()
+        domain.append(
+            "'|', ('carrier_ids', '=', False), ('carrier_ids', '=', carrier_id)"
         )
-    )
+        return domain
 
     def _get_release_channel_id_depends(self):
         depends = super()._get_release_channel_id_depends()
@@ -22,6 +22,11 @@ class SaleOrder(models.Model):
 
     def _get_release_channel_partner_date_domain(self):
         domain = super()._get_release_channel_partner_date_domain()
-        if self.carrier_id:
-            domain.append(("release_channel_id.carrier_ids", "in", self.carrier_id.ids))
+        if domain and self.carrier_id:
+            carrier_domain = [
+                "|",
+                ("release_channel_id.carrier_ids", "in", self.carrier_id.ids),
+                ("release_channel_id.carrier_ids", "=", False),
+            ]
+            domain = expression.AND([domain, carrier_domain])
         return domain
