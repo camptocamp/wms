bc92047a5117c4591a451b594a43616f3fad2a78
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-12-18 17:20:35 +0100

    sale_stock_release_channel_partner_by_date_delivery: disable carrier domain
    Disable temporary the carrier support in domain to filter specific channels.
    
    The customer is not yet able to configure the carriers on all release
    channels.

--- sale_stock_release_channel_partner_by_date_delivery/models/sale_order.py
+++ sale_stock_release_channel_partner_by_date_delivery/models/sale_order.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models
-from odoo.osv import expression
 
 
 class SaleOrder(models.Model):
@@ -20,13 +19,22 @@ class SaleOrder(models.Model):
         depends.append("carrier_id")
         return depends
 
-    def _get_release_channel_partner_date_domain(self):
-        domain = super()._get_release_channel_partner_date_domain()
-        if domain and self.carrier_id:
-            carrier_domain = [
-                "|",
-                ("release_channel_id.carrier_ids", "in", self.carrier_id.ids),
-                ("release_channel_id.carrier_ids", "=", False),
-            ]
-            domain = expression.AND([domain, carrier_domain])
-        return domain
+    # def _get_release_channel_partner_date_domain(self):
+    #     domain = super()._get_release_channel_partner_date_domain()
+    #     if domain and self.carrier_id:
+    #         carrier_domain = [
+    #             "|",
+    #             ("release_channel_id.carrier_ids", "in", self.carrier_id.ids),
+    #             ("release_channel_id.carrier_ids", "=", False),
+    #         ]
+    #         domain = expression.AND([domain, carrier_domain])
+    #     return domain
+    
+    def _compute_release_channel_id(self):
+        # pylint: disable=missing-return
+        super()._compute_release_channel_id()
+        for rec in self:
+            # Selected release channel and carrier have to be compatible
+            if rec.release_channel_id.carrier_ids and rec.carrier_id:
+                if rec.carrier_id not in rec.release_channel_id.carrier_ids:
+                    rec.release_channel_id = False
