aec33375dd43817b37f4037cad761abe98396e76
Jacques-Etienne Baudoux <je@bcim.be>
2025-03-27 17:52:27 +0100

    stock_release_channel: fix find rc
    When searching a channel for a picking, if the channel has a warehouse
    then it must match otherwise the picking type must match

--- stock_release_channel/models/stock_picking.py
+++ stock_release_channel/models/stock_picking.py
@@ -111,16 +111,22 @@ class StockPicking(models.Model):
         return [
             ("is_manual_assignment", "=", False),
             ("state", "!=", "asleep"),
-        ]
-
-    def _get_release_channel_possible_candidate_domain_picking(self):
-        return [
-            ("company_id", "=", self.company_id.id),
             "|",
             ("picking_type_ids", "=", False),
             ("picking_type_ids", "in", self.picking_type_id.ids),
+        ]
+
+    def _get_release_channel_possible_candidate_domain_picking(self):
+        # when a warehouse is defined on the channel, it must always match
+        # otherwise fallback on the picking type
+        return [
+            ("company_id", "=", self.company_id.id),
             "|",
+            "&",
             ("warehouse_id", "=", False),
+            "|",
+            ("picking_type_ids", "=", False),
+            ("picking_type_ids", "in", self.picking_type_id.ids),
             ("warehouse_id", "=", self.picking_type_id.warehouse_id.id),
         ]
 
